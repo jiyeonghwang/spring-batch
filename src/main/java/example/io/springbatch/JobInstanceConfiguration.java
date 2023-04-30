71fcd82c5294b0b0c035b6d006ec06c8b8428030
jiyeonghwang <chandle12340@gmail.com>
2023-04-30 17:32:57 +0900

    JobInstance 동작방식 확인

--- src/main/java/example/io/springbatch/JobInstanceConfiguration.java
+++ src/main/java/example/io/springbatch/JobInstanceConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class JobInstanceConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -33,7 +33,6 @@ public class JobConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step1 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -46,7 +45,6 @@ public class JobConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
